1edc0e2ec179c37c24ce123421447df2d8a90888
Reedgern <dmitriy250998@gmail.com>
2021-09-12 18:07:18 +0300

    added dynamic imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import Offers from "./components/Offers/Offers";
+// import Offers from "./components/Offers/Offers";
 import React from "react";
 import {Provider} from "react-redux";
 import store from "./redux/store";
 import {BrowserRouter, HashRouter, Route} from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
-import OfferPage from "./components/Offers/OfferPage/OfferPage";
+// import OfferPage from "./components/Offers/OfferPage/OfferPage";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import styles from './App.module.css';
+import Preloader from "./components/common/Preloader/Preloader";
+
+const Offers = React.lazy(() => import('./components/Offers/Offers'));
+const OfferPage = React.lazy(() => import('./components/Offers/OfferPage/OfferPage'));
 
 const App = (props) => {
     return (
@@ -18,8 +22,8 @@ const App = (props) => {
                     <Header/>
                     <main className={styles.mainContent}>
                         <Route exact path={'/'} render={() => <HomePage/>}/>
-                        <Route exact path={'/offers'} render={() => <Offers/>}/>
-                        <Route path={'/offers/:offerId'} render={() => <OfferPage/>}/>
+                        <Route exact path={'/offers'} render={() => <React.Suspense fallback={<Preloader/>}> <Offers/> </React.Suspense>}/>
+                        <Route path={'/offers/:offerId'} render={() => <React.Suspense fallback={<Preloader/>}> <OfferPage/> </React.Suspense>}/>
                     </main>
                     <Footer/>
                     </div>
